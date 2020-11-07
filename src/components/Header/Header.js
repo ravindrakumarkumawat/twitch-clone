93b31ebed4a994a4314e542c60e4c0bb308b018c
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-07 17:17:04 +0530

    creating header format

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+
+      </div>
+
+      <div className="header__center">
+
+      </div>
+      
+      <div className="header__right">
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
