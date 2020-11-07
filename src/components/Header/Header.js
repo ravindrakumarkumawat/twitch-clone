59d615b15d49116c62ae36b0ca1bab7fbf087f83
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-07 17:48:18 +0530

    header is created and added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,35 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-
+        <img className="header__leftLogo"
+          src="https://pngimg.com/uploads/twitch/twitch_PNG6.png"
+          alt="twitch logo"/>
+        <h2>Following</h2>
+        <h2>Browse</h2>
+        <div className="header_verticalLine"></div>
+        <h2>Esports</h2>
+        <h2>Music</h2>
+        <i className="fas fa-ellipsis-h"></i>
       </div>
 
       <div className="header__center">
-
+        <input type="text" className="" placeholder="Search"/>
+        <div className="header__centerLogoContainer">
+          <i className="fas fa-search"></i>
+        </div>
       </div>
-      
-      <div className="header__right">
 
+      <div className="header__right">
+        <div className="header__rightContainer">
+          <i className="fas fa-crown"></i>
+          <i className="fas fa-inbox"></i>
+          <i className="fas fa-comment-alt"></i>
+
+          <div className="header__rightBits">
+            <i class="fas fa-gem"></i>
+            <h4>Get Bits</h4>
+          </div>
+        </div>
       </div>
     </div>
   )
